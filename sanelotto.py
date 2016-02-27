293b834bba0f26efefec0cfa8a9d7a5cdc3f6664
Vladimir37 <vladimir_zapas@mail.ru>
2016-02-27 17:57:30 +0000

    Basis 1.1

--- sanelotto.py
+++ sanelotto.py
@@ -1,26 +1,23 @@
-import sys
-
 from routes.help import help
 from routes.generate import generate
 from routes.create import create
 from routes.start import start
 
-args = sys.argv
-args_count = len(args)
-
-if args_count == 1:
-    print('Incorrect command')
-    help()
-else:
-    command = args[1]
-    if(command == 'help'):
-        help()
-    elif(command == 'generate'):
-        generate()
-    elif(command == 'create'):
-        create(args)
-    elif(command == 'start'):
-        start(args)
-    else:
+def router(args):
+    args_count = len(args)
+    if args_count == 1:
         print('Incorrect command')
         help()
+    else:
+        command = args[1]
+        if(command == 'help'):
+            help()
+        elif(command == 'generate'):
+            generate()
+        elif(command == 'create'):
+            create(args)
+        elif(command == 'start'):
+            start(args)
+        else:
+            print('Incorrect command')
+            help()
