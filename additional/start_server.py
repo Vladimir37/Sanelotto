cadd6dce0e0fc04d02cc56814581cf65b5df7e82
Vladimir37 <vladimir_zapas@mail.ru>
2016-02-27 17:02:06 +0000

    Basis 1.0

--- additional/start_server.py
+++ additional/start_server.py
@@ -1,7 +1,8 @@
 import os
 import json
+import shutil
 
-from additional.signals import signal_ok, signal_err
+from additional.signals import signal_ok, signal_err, signal_done
 
 def start_server(current_dir):
     try:
@@ -23,16 +24,57 @@ def start_server(current_dir):
         # stop command
         if server_file['stop_commands']:
             try:
-                stop_sys = open(current_dir + '/' + server_file['commands_dir'] + '/stop.slfile')
+                stop_sys = open(current_dir + '/Sanelotto_server/' + server_file['commands_dir'] + '/stop.slfile', 'r')
                 for command in stop_sys:
                     os.system(prefix + command)
                 stop_sys.close()
+                signal_ok('Stop commands completed')
             except:
                 signal_err('Failed to run stop commands')
+                return False
+        # reload from GitHub
         os.system('cd ' + current_dir + '/' + server_file['name'] + ' && ' + prefix + 'git pull')
+        signal_ok('Project was reloaded from GitHub')
     # download project
     else:
         dl_addr = server_file['github_user'] + '/' + server_file['github_repo'] + ' -b ' + server_file['branch']
         os.system(prefix + 'git clone https://github.com/' + dl_addr)
+        signal_ok('Project was downloaded from GitHub')
 
-    #
+    # config overwrite
+    if server_file['rewrite_configs']:
+        # read __main__
+        try:
+            config_main_sys = open(current_dir + '/Sanelotto_server/' + server_file['configs_dir'] + '/' + '__main__.json', 'r')
+            config_main = config_main_sys.read()
+            config_main = json.loads(config_main)
+            config_main_sys.close()
+            signal_ok('__main__.json successfully read')
+        except:
+            signal_err('Failed to read __main__.json')
+            return False
+
+        # overwriting
+        for config_name in config_main:
+            try:
+                shutil.copyfile(current_dir + '/Sanelotto_server/' + server_file['configs_dir'] + '/' + config_name,
+                                current_dir + '/' + server_file['name'] + '/' + config_main[config_name])
+                signal_ok('Copying ' + config_name)
+            except:
+                signal_err('Failed to copy ' + config_name)
+                return False
+        signal_ok('Overwriting successfully completed!')
+
+        # launch
+        if server_file['start_commands']:
+            try:
+                stop_sys = open(current_dir + '/Sanelotto_server/' + server_file['commands_dir'] + '/start.slfile', 'r')
+                for command in stop_sys:
+                    os.system(prefix + command)
+                stop_sys.close()
+                signal_ok('Start commands completed')
+            except:
+                signal_err('Failed to run start commands')
+                return False
+
+        signal_done('Project successfully started')
