076d6cc833205c92be6a7db4d73f76bb58ce3c5b
Vladimir37 <vladimir_zapas@mail.ru>
2016-02-29 00:40:11 +0000

    Uninstalling 1.1

--- routes/uninstall.py
+++ routes/uninstall.py
@@ -4,10 +4,7 @@ import shutil
 from additional.signals import signal_err, signal_ok, signal_done
 
 def uninstall():
-    try:
-        home_dir = os.path.expanduser('~' + os.environ["SUDO_USER"])
-    except:
-        home_dir = os.path.expanduser('~')
+    sl_dir = '/usr/share/'
 
     question = input('For uninstall Sanelotto enter "uninstall": ')
     if question != 'uninstall':
@@ -25,12 +22,11 @@ def uninstall():
         signal_err('Failed to delete link. Permission denied or link not exist.')
         return False
 
-    if os.access(home_dir + '/.sanelotto', os.F_OK):
-        try:
-            shutil.rmtree(home_dir + '/.sanelotto')
-            signal_ok('Sanelotto directory was deleted')
-        except:
-            signal_err('Failed to delete Sanelotto directory')
-            return False
+    try:
+        shutil.rmtree(sl_dir + '/.sanelotto')
+        signal_ok('Sanelotto directory was deleted')
+    except:
+        signal_err('Failed to delete Sanelotto directory')
+        return False
 
     signal_done('Sanelotto was deleted')
